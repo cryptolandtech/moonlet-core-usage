515b45c76f879b23f46c9fa5029aefc6a6ed4459
Micky Socaci <micky@nowlive.ro>
2018-12-11 13:18:45 +0200

    Basic Usage Example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import { Wallet, Blockchains } from 'moonlet-core';
+import { Ethereum } from 'moonlet-core/dist/lib/blockchain/ethereum/class.index';
 
 // find supported blockchains
 console.log( "Supported Blockchains:" );
 for(const blockchain in Blockchains) {
     console.log( Blockchains[blockchain] );
 }
+console.log();
 
 // Use the wallet
-console.log( "Use the wallet:" );
+console.log( "Using the wallet:" );
 const myWallet = new Wallet();
-console.log( "Generated mnemonics:", myWallet.mnemonics );
+
+// initialise used blockchains
+myWallet.loadBlockchain( Ethereum );
+
+console.log( "Generated mnemonics:", myWallet.mnemonics, "\n" );
 
 // create an ethereum account
-const bc = myWallet.getBlockchain( Blockchains.ETHEREUM );
-bc.createAccount();
-const accs = bc.getAccounts();
-console.log( accs[0].address );
+const ethbc = myWallet.getBlockchain( Blockchains.ETHEREUM );
+ethbc.createAccount();
+ethbc.createAccount();
+
+console.log( "Generated account addresses:");
+
+myWallet.getAccounts(Blockchains.ETHEREUM).forEach( data => {
+    console.log( data.address  );
+});
+console.log();
